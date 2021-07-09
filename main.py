0a631b911b31cb264d2b2cde229c2948e9ef8522
Akshaya H <81792372+my-my414@users.noreply.github.com>
2021-07-09 20:16:03 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+# python program to conveeert temperature in celsius to fahrenheit
+
+#change this value for different result
+
+celsius=34.6
+
+#calculate fahrenheit
+
+fahrenheit=(celsius *1.8)+32
+
+print("%0.1f degree celsius is equal to %0.1f degree fahrenheit" %(celsius,fahrenheit))
